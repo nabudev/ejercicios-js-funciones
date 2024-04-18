b400625ea6e78ee1042a74b566b6371e991907ca
nahueldev <nabudev01@gmail.com>
2024-04-18 16:14:08 -0300

    subiendo ejercicio 7

---
+++ ejercicio-7/script.js
@@ -0,0 +1,21 @@
+function Doble(n){
+    resultado = n*2;
+    return resultado;
+}
+function Cuadrado(n){
+    resultado= n*n;
+    return resultado;
+};
+
+function ImprimirValores(n){
+    console.log(Doble(n));
+    console.log(Cuadrado(n));
+};
+
+ImprimirValores(5);
+
+
+
+
+
+
